777e5cd4a7ddeb3afe00b2e81db817ec6bdef83f
danielleep <113351600+danielleep@users.noreply.github.com>
2024-01-15 16:01:44 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,6 +2,7 @@ public class ArrayOps
 {
     public static void main(String[] args) 
     {
+        
     }
     
     public static int findMissingInt (int [] array) 
@@ -9,12 +10,12 @@ public class ArrayOps
         int[] fix=new int[array.length+1];
         for(int j=0;j<=fix.length;j++)
         {
-            if(j<=array.length)
+            if(j<array.length)
             {
                 fix[array[j]]=1;               
             }
         }
-        for(int i =0;i<=fix.length;i++)
+        for(int i=0;i<=fix.length;i++)
         {
             if(fix[i]==0)
             {
@@ -26,20 +27,17 @@ public class ArrayOps
 
     public static int secondMaxValue(int [] array) 
     {
-        int max;
-        int secondMax;
-        if(array[i]<array[i+1])
-            {
-                max=array[i+1];
-                secondMax=array[i];
-            }
-        for (int i=0; i<=array.length;i++) 
+        int max=array[0];
+        int secondMax=array[0];
+        for (int i=0; i<array.length;i++) 
         {
-            if(array[i]>max)
+            if(array[i]>=max)
             {
+                secondMax=max;
                 max=array[i];
+                
             }
-            else if(array[i]>secondMax)
+            if(array[i]<max && array[i]>secondMax)
             {
                 secondMax = array[i];
             }
@@ -50,7 +48,7 @@ public class ArrayOps
     public static boolean containsTheSameElements(int [] array1,int [] array2) 
     {
         int counter =0;
-        for(int i=o;i<=array1;i++)
+        for(int i=0;i<array1.length;i++)
         {
             if (contain(array2, array1[i])==true)
             {
@@ -61,12 +59,12 @@ public class ArrayOps
         {
             return true;
         }
-        else return false;
+        return false;
     }
 
     public static boolean contain (int[] array, int x) 
     {
-        for(int i =o; i<=array.length;i++)
+        for(int i =0; i<array.length;i++)
         {
             if (array[i]==x)
             {
@@ -76,8 +74,10 @@ public class ArrayOps
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
-        if (array[1]<array[2])
+
+    public static boolean isSorted(int [] array) 
+    {
+        if (array[0]<array[1])
         {
             for(int i =0;i<array.length;i++)
             {
@@ -89,7 +89,7 @@ public class ArrayOps
         }
         else
         {
-            for(int i =0;i<array.length;i++)
+            for(int i=0;i<array.length;i++)
             {
                 if(array[i]<array[i+1])
                 {
